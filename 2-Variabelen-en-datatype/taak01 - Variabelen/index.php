adb11f6a8e858a02d276b60da9cc4dfec4d5cfd0
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2021-02-05 13:55:46 +0100

    change

--- 2-Variabelen-en-datatype/taak01 - Variabelen/index.php
+++ 2-Variabelen-en-datatype/taak01 - Variabelen/index.php
@@ -4,8 +4,10 @@
 
 
 <?php
-    $voornaam = "maxim";
-    var_dump($voornaam);
-?>
+  $myMovies = array("Spiderman", "Batman", "The Joker");
+  echo $myMovies[0];
+  echo $myMovies[1];
+  echo $myMovies[2];
+  ?>
 </body>
 </html> 
